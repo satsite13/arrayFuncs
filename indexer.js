9f193cb90baa60e082dea693329aea310255c10b
satsite13 <s.site13@gmail.com>
2021-11-25 07:33:52 -0700

    added next funcs

--- indexer.js
+++ indexer.js
@@ -5,10 +5,39 @@ module.exports = {
 
    AvgArray: function avgArray(arr){
       return arr.reduce((a,b) => a+b)/(arr.length === 0? 1: arr.length);
-   }
+   },
+
+   MaxVal: function findMax(arr){
+      if(arr.length <=0) "Empty Array";
+      return Math.max(...arr);
+   },
+
+   MinVal: function findMin(arr){
+      if(arr.length <=0) "Empty Array";
+      return Math.min(...arr);
+   },
+
+   AToS: function aToS(arr){
+      let res = [];
+      for(let i = 0; i<arr.length; i++){
+         if(res.indexOf(arr[i]) === -1) res.push(arr[i]);
+      }
+      res.sort((a,b) => a-b);
+      return res;
+   },
+
 }
 
 
+function tester(arr){
+   let res = [];
+   for(let i = 0; i<arr.length; i++){
+      if(res.indexOf(arr[i]) === -1) res.push(arr[i]);
+   }
+   res.sort((a,b) => a-b);
+   return res;
+}
 
+console.log(tester([1,111,20,21,33,4,8,22,1120,32,34]))
 
 
