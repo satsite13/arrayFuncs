c028e64ede3d7c50a36607f4d84615f6af4261bf
satsite13 <s.site13@gmail.com>
2021-11-25 07:52:10 -0700

    added string func

--- indexer.js
+++ indexer.js
@@ -1,29 +1,37 @@
 module.exports = {
-   SumArray: function sumArray(arr){
+   SumArray: function(arr){
       return arr.reduce(((a,b) => a+b), 0);
    },
 
-   AvgArray: function avgArray(arr){
+   AvgArray: function(arr){
       return arr.reduce((a,b) => a+b)/(arr.length === 0? 1: arr.length);
    },
 
-   MaxVal: function findMax(arr){
+   MaxVal: function(arr){
       if(arr.length <=0) "Empty Array";
       return Math.max(...arr);
    },
 
-   MinVal: function findMin(arr){
+   MinVal: function(arr){
       if(arr.length <=0) "Empty Array";
       return Math.min(...arr);
    },
 
-   AToS: function aToS(arr){
+   UniqArraySort: function(arr){
       let res = [];
       for(let i = 0; i<arr.length; i++){
          if(res.indexOf(arr[i]) === -1) res.push(arr[i]);
       }
       res.sort((a,b) => a-b);
       return res;
+   },
+
+   UniqStringArray: function(arr){
+      let res = [];
+      for(let i = 0; i<arr.length; i++){
+         if(res.indexOf(arr[i]) === -1) res.push(arr[i]);
+      }
+      return res.sort();;
    },
 
 }
